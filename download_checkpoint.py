4035e12ed953c409d8c029a9ce9bf6037f1590a0
rhogard <rhogard@paradowski.com>
2023-03-24 13:28:40 -0500

    convert indentation to tabs

--- download_checkpoint.py
+++ download_checkpoint.py
@@ -17,22 +17,22 @@ HF_TOKEN = os.environ.get('HF_TOKEN', '')
 CHUNK_SIZE = 1024 * 1024
 
 def get_filename(MODEL_URL):
-    if '.safetensors' in MODEL_URL:
-        return 'models/Stable-diffusion/model.safetensors'
-    else:
-        return 'models/Stable-diffusion/model.ckpt'
+	if '.safetensors' in MODEL_URL:
+		return 'models/Stable-diffusion/model.safetensors'
+	else:
+		return 'models/Stable-diffusion/model.ckpt'
 
 def check_model_file(filename):
-    file_size_mb = round(os.path.getsize(filename) / (1024 * 1024))
-    if file_size_mb < 100:
-        print(f'The downloaded file is only {file_size_mb} MB and does not appear to be a valid model.')
-        sys.exit(1)
+	file_size_mb = round(os.path.getsize(filename) / (1024 * 1024))
+	if file_size_mb < 100:
+		print(f'The downloaded file is only {file_size_mb} MB and does not appear to be a valid model.')
+		sys.exit(1)
 
 def download_s3_file():
 	print('download_s3_file')
 	filename = get_filename('')
 	print("Model URL:", (BUCKET_NAME + '/' + CKPT_OBJECT_KEY))
-    print("Download Location:", filename)
+	print("Download Location:", filename)
 	if not AWS_REGION or not AWS_ACCESS_KEY or not AWS_SECRET_ACCESS_KEY:
 		print('AWS_REGION AWS_ACCESS_KEY or AWS_SECRET_ACCESS_KEY not provided')
 	time.sleep(1)
@@ -43,47 +43,47 @@ def download_s3_file():
 	check_model_file(filename)
 
 def download_hf_file():
-    filename = get_filename(MODEL_URL)
-    print("Model URL:", MODEL_URL)
-    print("Download Location:", filename)
-    if not HF_TOKEN:
-        print("A Huggingface token was not provided.")
-    else:
-        print("Using Huggingface authentication token.")
-    time.sleep(1)
-    headers = {'Authorization': f'Bearer {HF_TOKEN}'}
-    response = requests.get(MODEL_URL, headers=headers, stream=True)
-    response.raise_for_status()
-    with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
-        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
-                progress.update(len(chunk))
-    check_model_file(filename)
+	filename = get_filename(MODEL_URL)
+	print("Model URL:", MODEL_URL)
+	print("Download Location:", filename)
+	if not HF_TOKEN:
+		print("A Huggingface token was not provided.")
+	else:
+		print("Using Huggingface authentication token.")
+	time.sleep(1)
+	headers = {'Authorization': f'Bearer {HF_TOKEN}'}
+	response = requests.get(MODEL_URL, headers=headers, stream=True)
+	response.raise_for_status()
+	with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
+		for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+			if chunk:
+				f.write(chunk)
+				progress.update(len(chunk))
+	check_model_file(filename)
 
 def download_other_file():
-    filename = get_filename(MODEL_URL)
-    print("Model URL:", MODEL_URL)
-    print("Download Location:", filename)
-    time.sleep(1)
-    response = requests.get(MODEL_URL, stream=True)
-    response.raise_for_status()
-    with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
-        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
-                progress.update(len(chunk))
-    check_model_file(filename)
+	filename = get_filename(MODEL_URL)
+	print("Model URL:", MODEL_URL)
+	print("Download Location:", filename)
+	time.sleep(1)
+	response = requests.get(MODEL_URL, stream=True)
+	response.raise_for_status()
+	with open(filename, 'wb') as f, tqdm(desc="Downloading", unit="bytes", total=int(response.headers.get('content-length', 0))) as progress:
+		for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+			if chunk:
+				f.write(chunk)
+				progress.update(len(chunk))
+	check_model_file(filename)
 
 def download_model():
-    if BUCKET_NAME:
-        download_s3_file()
-    elif 'huggingface.co' in MODEL_URL:
-        if '/blob/' in MODEL_URL:
-            MODEL_URL = MODEL_URL.replace('/blob/', '/resolve/')
-        download_hf_file()
-    else:
-        download_other_file()
+	if BUCKET_NAME:
+		download_s3_file()
+	elif 'huggingface.co' in MODEL_URL:
+		if '/blob/' in MODEL_URL:
+			MODEL_URL = MODEL_URL.replace('/blob/', '/resolve/')
+		download_hf_file()
+	else:
+		download_other_file()
 
 if __name__ == "__main__":
-    download_model()
+	download_model()
